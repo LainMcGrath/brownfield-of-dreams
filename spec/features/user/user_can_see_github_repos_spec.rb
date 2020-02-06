750e1883a5e210751050eab2d79afe47bdf7c615
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-02-06 15:04:29 -0700

    Write test so user can see github repos

---
+++ spec/features/user/user_can_see_github_repos_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User show page' do
+  scenario 'can login to github' do
+    VCR.use_cassette('github login') do  
+      user = create(:user, token: ENV['GITHUB_TOKEN'])
+  
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  
+      visit dashboard_path
+      expect(current_path).to eq(dashboard_path)
+
+      expect(page).to have_css(".github-repos", count: 5)
+    end
+  end
+end

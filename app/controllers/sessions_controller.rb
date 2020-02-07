12c633097c3bfe5ebbbd685ca52c0cdaacef3192
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-06 20:53:28 -0700

    Refactor sessions controller to move github to update method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,27 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if request.env['PATH_INFO'] == "/login"
-      if (user = User.find_by(email: params[:session][:email])) && user&.authenticate(params[:session][:password])
-        session[:user_id] = user.id
-      else
-        flash[:error] = 'Looks like your email or password is invalid'
-        render :new
-      end
+    if (user = User.find_by(email: params[:session][:email])) && user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
     else
-      user_info = request.env['omniauth.auth']
-      current_user.uid = user_info[:uid]
-      current_user.token = user_info[:credentials][:token]
-      current_user.login = user_info[:extra][:raw_info][:login]
-      current_user.save!
-      session[:github] = current_user.login
+      flash[:error] = 'Looks like your email or password is invalid'
+      render :new
     end
     redirect_to dashboard_path
   end
 
   def update
-    user = User.find(session[:user_id])
-    User.github_login(params, user)
+    User.update_user(request.env['omniauth.auth'], current_user)
+    session[:github] = current_user.login
     redirect_to dashboard_path
   end
 

c419b1855f5633505712dddd713c164a8c42c35a
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-06 20:54:01 -0700

    Refactor github_login (now update_user) method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,25 +8,11 @@ class User < ApplicationRecord
   enum role: %i[default admin]
   has_secure_password
 
-  def self.github_login(session_params, user)
-    # client_id = '13bb7d4ef6b3c23576d2'
-    # client_secret = 'd1ba88fb71c5a1f99156628b3aa15a68741fa9e0'
-    code = session_params[:code]
-
-    GithubService.new.login(code)
-
-
-    # response = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
-    # token_params = response.body.split("&").first.split('=')
-    # token = { token_params[0] => token_params[1] }
-    # oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token["access_token"]}")
-
-    # auth = JSON.parse(oauth_response.body)
-
-    user.login = auth['login']
-    user.uid = auth['id']
-    user.token = token["access_token"]
-    user.save
+  def self.update_user(params, current_user)
+    current_user.uid = params[:uid]
+    current_user.token = params[:credentials][:token]
+    current_user.login = params[:extra][:raw_info][:login]
+    current_user.save!
   end
 
   def self.fetch_repos(user_id)

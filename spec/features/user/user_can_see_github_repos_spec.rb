edbd64609cb764d42754b766181d4f335e03217d
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-07 17:12:29 -0700

    Add user show page test and commented out SSO test

--- spec/features/user/user_can_see_github_repos_spec.rb
+++ spec/features/user/user_can_see_github_repos_spec.rb
@@ -1,17 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe 'User show page' do
-  scenario 'can login to github', :vcr do
-    user = create(:user, token: ENV['GITHUB_TOKEN'])
-    page.set_rack_session(github: user.email)
+feature 'User show page' do
+  scenario 'can see show page after logging in' do
+    user = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_path
-    expect(current_path).to eq(dashboard_path)
 
-    expect(page).to have_css(".github-repos", count: 5)
+    expect(page).to have_content("#{user.first_name}")
+    expect(page).to have_content("#{user.last_name}")
+    expect(page).to have_content("#{user.email}")
+    expect(page).to_not have_content('GitHub Repositories')
+    click_button('Log Out')
+    expect(current_path).to eq(root_path)
   end
+
+  # scenario 'can login to github SSO', :vcr do
+  #   user = create(:user, token: ENV['GITHUB_TOKEN'])
+  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  #
+  #   visit dashboard_path
+  #
+  #   expect(page).to_not have_content('GitHub Repositories')
+  #   stub_omniauth
+  #   # require "pry"; binding.pry
+  #   click_button('Log in to GitHub')
+  #   page.set_rack_session(github: user.email)
+  #   expect(current_path).to eq(dashboard_path)
+  #   expect(page).to have_content('Github Repositories')
+  #   expect(page).to have_css(".github-repos", count: 5)
+  # end
 
   scenario 'user only sees their own repos', :vcr do
     user_1 = create(:user, token: ENV['GITHUB_TOKEN'])
@@ -31,14 +50,12 @@ RSpec.describe 'User show page' do
     user_2 = create(:user, token: ENV['JORDANS_GITHUB'])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_2)
     page.set_rack_session(github: user_2.email)
-    # require "pry"; binding.prye
-    visit dashboard_path
 
+    visit dashboard_path
     expect(page).to have_link("activerecord-obstacle-course")
     expect(page).to have_link("adopt_dont_shop")
     expect(page).to have_link("backend_module_0_capstone")
     expect(page).to have_link("battleship")
     expect(page).to have_link("black_thursday_lite")
-
   end
 end

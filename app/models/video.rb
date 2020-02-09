91f2babcef4a05a25472107fbc894c3ade62d4ab
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-09 12:00:20 -0700

    remove video validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,8 +2,4 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
-
-  validates_presence_of :title
-  validates_presence_of :description
-  validates_presence_of :youtube_id
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,10 +6,4 @@ RSpec.describe Video do
     it {should have_many(:users).through(:user_videos)}
     it {should belong_to :tutorial}
   end
-
-  describe 'validations' do
-    it { should validates_presence_of :title }
-    it { should validates_presence_of :description }
-    it { should validates_presence_of :youtube_id }
-  end
 end

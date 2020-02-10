9beb95308b8899385ee16acf1ea1eceeaae49d19
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-02-10 12:54:32 -0700

    Finish tutorial hiding from visitors

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,11 +1,19 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    if tutorial.classroom == false || current_user
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    else
+      render file: "/public/404"
+    end
   end
 
   def update
     tutorial = Tutorial.find(params[:id])
     tutorial.tags.create(name: params[:tutorial][:tag_list])
+  end
+
+  def index
+    @tutorials = Tutorial.all
   end
 end
--- spec/features/vistors/visitor_cannot_see_classroom_tutorials_spec.rb
+++ spec/features/vistors/visitor_cannot_see_classroom_tutorials_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature 'Visitor on the tutorial index page' do
-  scenario 'cannot see a tutorial if it is classroom content' do
-    prework_tutorial = Tutorial.create!({
+  before :each do
+    @prework_tutorial = Tutorial.create!({
       "title"=>"Back End Engineering - Prework",
       "description"=>"Videos for prework.",
       "thumbnail"=>"https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg",
@@ -10,15 +10,15 @@ RSpec.feature 'Visitor on the tutorial index page' do
       "classroom"=>false
     })
     
-    m1_tutorial = Tutorial.create!({
+    @m1_tutorial = Tutorial.create!({
       "title"=>"Back End Engineering - Module 1",
       "description"=>"Videos related to Mod 1.",
       "thumbnail"=>"https://i.ytimg.com/vi/tZDBWXZzLPk/hqdefault.jpg",
       "playlist_id"=>"PL1Y67f0xPzdNsXqiJs1s4NlpI6ZMNdMsb",
       "classroom"=>true
     })
-
-    m3_tutorial = Tutorial.create!({
+  
+    @m3_tutorial = Tutorial.create!({
       "title"=>"Back End Engineering - Module 3",
       "description"=>"Video content for Mod 3.",
       "thumbnail"=>"https://i.ytimg.com/vi/R5FPYQgB6Zc/hqdefault.jpg",
@@ -26,9 +26,15 @@ RSpec.feature 'Visitor on the tutorial index page' do
       "classroom"=>false,
       "tag_list"=>["Internet", "BDD", "Ruby"],
     })
+  end
 
+  scenario 'cannot see a tutorial if it is classroom content on the root path or tutorials index' do
     visit root_path
     
+    expect(page).to have_css(".tutorial", count: 2)
+
+    visit tutorials_path
+
     expect(page).to have_css(".tutorial", count: 2)
 
     user = create(:user)
@@ -37,5 +43,23 @@ RSpec.feature 'Visitor on the tutorial index page' do
     visit root_path
     
     expect(page).to have_css(".tutorial", count: 3)
+
+    visit tutorials_path
+
+    expect(page).to have_css(".tutorial", count: 3)
+  end
+
+  scenario 'cannot visit a tutorial show page that is classroom content and they are not logged in' do
+    visit tutorial_path(@m1_tutorial)
+
+    expect(page).to have_content("The page you were looking for doesn't exist")
+
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit tutorial_path(@m1_tutorial)
+
+    expect(page).to_not have_content("The page you were looking for doesn't exist")
+    expect(page).to have_content("Back End Engineering - Module 1")
   end
 end
